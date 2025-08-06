ed230cce94679b198464a4c6aa7b47c739eb61e1
Aoyagi Satoshi <aoyagi@example.com>
2025-08-06 10:35:31 +0900

    最終課題その1を行いました。

--- practice/src/main/java/com/example/demo/service/ContactServiceImpl.java
+++ practice/src/main/java/com/example/demo/service/ContactServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -30,4 +32,8 @@ public class ContactServiceImpl implements ContactService {
 		contactRepository.save(contact);
 	}
 
+	@Override
+	public List<Contact> getAllContact() {
+		return contactRepository.findAll();
+	}
 }
