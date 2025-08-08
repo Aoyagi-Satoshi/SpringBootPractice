1f1a4e9524a879b572f78a6ac0135bcb844f209a
Aoyagi Satoshi <aoyagi@example.com>
2025-08-08 18:23:27 +0900

    最終課題その2を行いました。

--- practice/src/main/java/com/example/demo/service/ContactService.java
+++ practice/src/main/java/com/example/demo/service/ContactService.java
@@ -4,9 +4,18 @@ import java.util.List;
 
 import com.example.demo.entity.Contact;
 import com.example.demo.form.ContactForm;
+import com.example.demo.form.EditContactForm;
 
 public interface ContactService {
 	void saveContact(ContactForm contactForm);
 
 	List<Contact> getAllContact();
+
+	Contact getDetailContact(Long id);
+
+	Contact getEdit(Long id);
+
+	void updateContact(EditContactForm editContactForm);
+
+	void delete(Long id);
 }
